5a1161a0bc773bb8e69ca2111e1edb0bd9a42cb1
Juan Sebastian Vargas Torres <libros123bmw@gmail.com>
2022-09-12 01:43:29 -0500

    Add the projects info

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
+data='{"ai_projects":[{"title":"Análisis de delitos sexuales, intrafamiliares, \
+secuestro y homicidios en Colombia","imageUrl":"https://cdn.techinasia.com/wp-content/uploads/2021/09/1630643087_fi-alibabas-sexual-harassment-allegations.png","description":"Muchos gobiernos alrededor del mundo han promovidola iniciativa de datos abiertos habilita a cualquier ciudadano tener acceso a datos de dominio público. \
+ De esta forma, los ciudadanos pueden participar en la creación de políticas públicas, hacer seguimiento al gasto público y en general tener visibilidad de variados aspectos de índole público. Colombia no es la excepción. En el portal de datos abiertos datos.gov.co los ciudadano pueden acceder a datos oficiales y no oficiales de diferentes categorías, entidades y zonas del país. \
+ Adicionalmente, también pueden aportar a la comunidad subiendo nuevos datos o análisis sobre datos ya existentes. Al día de hoy existen casi 6 mil conjuntos de datos, más de 20 mil visualizaciones y 935 entidades.","link":"https://github.com/Sebas102507/taller1_gestion_de_datos"},{"title":"Redes Neuronales (Perceptrón y Adeline desde cero con numpy","imageUrl":"https://datascientest.com/es/wp-content/uploads/sites/7/2021/03/perceptron-formule.png","description":"Este notebook tiene como objetivo familiarizar al estudiante con los modelos de redes neuronales básicos: Perceptrón y ADALINE. \
+ Se prueba el desempeño con 2 conjuntos de  datos: linealmente separables y no linealmente separables.","link":"https://github.com/Sebas102507/NeuralNetwork_Perceptron"},{"title":"Redes Neuronales poco profundas con sklearn","imageUrl":"https://miro.medium.com/max/1400/1*3fA77_mLNiJTSgZFhYnU0Q.png","description":"En este notebook vamos a explorar implementaciones de redes neuronales feedforward en usando la librería scikit-learn y se propone el enunciado del taller 2 del curso.","link":"https://github.com/Sebas102507/RedesNeuronalesPocoProfundas"},{"title":"jobClassificationModel","imageUrl":"https://cdn.corporatefinanceinstitute.com/assets/how-to-quit-a-job1.jpeg","description":"Una empresa que se dedica al Big Data y a la Ciencia de Datos quiere contratar a científicos de datos entre las personas que superen con éxito una serie de cursos que imparte la empresa. \
+ Muchas personas se inscriben a su formación. La empresa quiere saber cuáles de los candidatos inscritos quieren realmente trabajar para la empresa después de la formación o buscar un nuevo empleo. Conocer esto le ayudará a reducir costos y tiempo pues permitirá tener una categorización de los candidatos.Reto El reto que usted y su equipo deben enfrentar es el de generar modelos de analítica que permitan reconocer los factores que afectan la decisión del empleado de dejar o no su empleo actual. Así mismo, un modelo que prediga para un conjunto de datos de prueba la probabilidad de que un empleado deje su trabajo actual. Para hacer este análisis se dispone de la información con datos demográficos, de educación y de experiencia que los candidatos diligencian durante el registro. \
+ El supuesto es que este conjunto permitirá entender los factores que llevan a una persona a dejar su trabajo actual.","link":"https://github.com/Sebas102507/jobClassificationModel"},{"title":"Red Neuronal desde cero con Numpy","imageUrl":"https://www.niser.ac.in/~smishra/teach/cs460/2020/lectures/lec19/Perceptron.jpg","description":"Este notebook muestra el algoritmo de entrenamiento de una red neural desde cero usando numpy, implementando feedfoward y backpropagation.","link":"https://github.com/Sebas102507/NeuralNetworkFromCeroWithNumpy"},{"title":"BicyclesClustering","imageUrl":"https://miro.medium.com/max/1400/1*Ag5L08TUqLGKmhFvm2StTQ.png","description":"Objetivo: El objetivo de este cuaderno es aplicar el algoritmo de clustering a un conjunto de datos e interpretar los resultados:Para este ejercicio se estará utilizando el conjunto de datos AdventureWorks. Es un conjunto de datos de clientes, compradores de bicicletas. ","link":"https://github.com/Sebas102507/bicyclesClustering"},{"title":"Probando métodos de ensamble","imageUrl":"https://miro.medium.com/max/773/1*Bic1BVl02k2evh0MmebhIQ.png","description":"Objetivo: El objetivo de este cuaderno es familiarizarse con las técnicas de ensamble, identificar en qué se diferencian entre ellas y lograr comparar sus resultados.","link":"https://github.com/Sebas102507/metodosEnsamble"},{"title":"Regresión lineal desde cero con Numpy","imageUrl":"https://cdn.wallstreetmojo.com/wp-content/uploads/2022/06/linear-regression-3.jpg", \
+ "description":"En este notebook vamos a explorar la implementación de un modelo de regresión lineal usando Python bpasico y la librería para la clase de aprendizaje de Máquina de la materia de maestría en Inteligencia Artificial de la PUJ.","link":"https://github.com/Sebas102507/linearRegressionML"},{"title":"Fifa Data Analysis","imageUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS67gplgsVPtEhplIdzkbLD_wEgI5mIYOLoHfaqTiBQy1jDJchMhesecE7Qdib1o-QulMo&usqp=CAU","description":"Este notebook tiene como objetivo realizar una exploración y limpieza de los datos del videojuego Fifa y finalmente realizar una serie de conlusiones.","link":"https://github.com/Sebas102507/fifaDataExploration"},{"title":"Implementación de regresión logística desde cero con numpy","imageUrl":"https://datatab.net/assets/tutorial/LogRegPlot.png","description":"Para este taller se buscó implementar una regresión logística desde cero usando numpy.Materia de Machine Learning de la maestría en Inteligencia Artificial en la PUJ.","link":"https://github.com/Sebas102507/logisticRegression"},{"title":"Comparación de modelos a partir de la curva de ROC","imageUrl":"https://www.researchgate.net/publication/339868711/figure/fig4/AS:941414187810828@1601462041991/Receiver-operating-characteristic-ROC-curve-analysis-was-conducted-to-discriminate-BOTs.png","description":"Objetivo: El objetivo de este cuaderno es familiarizarse con la comparación de clasificadores 1) Cargar librerías 2) Cargar los Datos 3) División en Entrenamiento y pruebas 4) \
+ Entrenamiento y prueba de los clasificadores 5) Comparación de los Modelos usando la curva ROC","link":"https://github.com/Sebas102507/ROC_Curve"},{"title":"Implementación de árboles de decisión","imageUrl":"https://hands-on.cloud/wp-content/uploads/2022/01/decision-tree-using-Python-decisiontree-1024x576.png","description":"Objetivo:  El objetivo de este cuaderno es familiarizarse con la técnica supervisada de árboles de decisión a través del entrenamiento, evaluación y uso para predicción de un árbol.1)Cargar librerías 2)Cargar los Datos 3)Explorar los Datos 4)Preparar los datos 5)División del conjunto de datos en entrenamiento y pruebas 6)Entrenamiento y prueba del Arbol de Decision 7)Calcular métricas de desempeño8)Visualizar el árbol 9)Aplicar pre-poda 10)Optimizar hiper parámetros","link":"https://github.com/Sebas102507/DecisionTree"},{"title":"Análisis de los contratos públicos en Colombia","imageUrl":"https://www.funcionpublica.gov.co/eva/admon/files/empresas/ZW1wcmVzYV83Ng==/imagenes/c4989a00abcf21e62b1a8461582f8a3616efdbea.jpg","description":"Análisis de los contratos públicos en Colombia, join con datos de financiación de campañas con el fin de analizar posibles contratos corruptos y la relación entre la financiación de campañas y la adquisión de licitaciones","link":"https://github.com/Sebas102507/publicContractsColombia"},\
+ {"title":"Exploración de los datos del Centro Nacional de Estadísticas de Salud de EE. UU.","imageUrl":"https://bloximages.newyork1.vip.townnews.com/midfloridanewspapers.com/content/tncms/assets/v3/editorial/8/24/8249216c-82d1-11ec-ba02-ff29a948c557/61f841a8b11af.image.jpg","description":"Estos son datos de encuestas recopilados por el Centro Nacional de Estadísticas de Salud de EE. UU. (NCHS, por sus siglas en inglés) que ha llevó a cabo una serie de encuestas sobre salud y nutrición desde principios de la década de 1960. Desde 1999 aproximadamente 5.000 personas de todas las edades son entrevistadas en sus hogares cada año y completan la encuesta de salud componente de examen de la encuesta. El examen de salud se lleva a cabo en un examen móvil. centro (MEC). La población objetivo de NHANES es “la población residente civil no institucionalizada de los Estados Unidos”. Nuestro conjunto de datos se deriva de los datos disponibles para los años de muestra 2009-2010 y 2011-2012 y tiene aproximadamente 10.000 observaciones (personas) y unas 70 variables. NHANES, (Encuestas de Examen Nacional de Salud y Nutrición de los Estados Unidos), utiliza encuestas complejas diseños (ver http://www.cdc.gov/nchs/data/series/sr_02/sr02_162 pdf) que sobremuestrean ciertos subpoblaciones como minorías raciales.","link":"https://github.com/Sebas102507/ExploracionNCHS"}, \
+ {"title":"Modelo de optimización lineal","imageUrl":"https://col.jrs.net/wp-content/uploads/sites/14/2021/09/imagen_2021-09-07_125319.png","description":"En el problema planteado del presente informe se busca  minimizar los costos a través de la asignación de recursos por parte del JRS, por lo cual, partiendo del contexto del problema, se sabe que el JRS comenzó a recibir una serie de donaciones por parte de distintas entidades: PRM,Unión Europea, Cancillería, GIZ","link":"https://www.overleaf.com/read/vktsdqyfzmrc"}]}'
+
+
 window.onload= function(){
     document.querySelectorAll('.navegationBarItem').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
@@ -8,4 +21,54 @@ window.onload= function(){
         });
     });
     console.log("Ready");
+    setProjectsInfo()
+
+}
+
+
+function setProjectsInfo(){
+    const obj = JSON.parse(data)
+    console.log(obj.ai_projects);
+    setAIProjects(obj.ai_projects)
+}
+
+function setAIProjects(projects){
+    let i=0
+    let j=0
+    let tr;
+    table= document.getElementById("aiprojectTable")
+    body= document.createElement("tbody")
+    for (p in projects) {
+        console.log(p);
+        if((i)%3==0 || i==0){
+            j=j+1
+            console.log("NEW TR")
+            tr= document.createElement("tr")
+        }
+        td= document.createElement("td")
+        td.appendChild(getProjectElement(projects[p]))
+        tr.appendChild(td)
+        body.appendChild(tr)
+        i=i+1
+      }
+      table.appendChild(body)
+}
+
+function getProjectElement(p){
+    div= document.createElement("div")
+    div.className= "tableItem"
+    img= document.createElement("img")
+    img.src=p.imageUrl
+    h4= document.createElement("h4")
+    h4.innerHTML=p.title
+    par= document.createElement("p")
+    par.innerHTML=p.description
+    a= document.createElement("a")
+    a.href=p.link
+    a.innerHTML="Repositorio"
+    div.appendChild(img)
+    div.appendChild(h4)
+    div.appendChild(par)
+    div.appendChild(a)
+    return div
 }
